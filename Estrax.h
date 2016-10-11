8b48bd84ef3ca4d6e59b87886eb20e85eb853694
Dimezske <dimezske@gmail.com>
2016-10-11 20:52:32 +1100

    This is the Header File
    This Header File Contains 3 global variables that use extern to tell the various files in this project, it also includes the function prototypes as forward references

---
+++ Estrax.h
@@ -0,0 +1,14 @@
+#ifndef ESTRAX_H_INCLUDED 
+#define ESTRAX_H_INCLUDED
+
+extern int Hamburgers;
+extern int Cheeseburgers;
+extern int Angusburgers;
+
+void inputMenu();
+void eattinghamBurgers();
+void eattingcheeseBurgers();
+void eattingangusBurgers();
+
+
+#endif //ESTRAX_H_INCLUDED
